e44601c9b005f77e93485a4b273b8e3dbb9e4549
AKSH PATEL <akshpatel26902@gmail.com>
2026-01-18 01:35:00 +0530

    fast api created and tested sucessfully

---
+++ api/main.py
@@ -0,0 +1,14 @@
+## DS614-Faculty-Finder/api/main.py
+from fastapi import FastAPI
+
+import os 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from api.routes import router 
+
+app=FastAPI(title="Faculty Finder API",description="API to fetch faculty details",version="1.0.0")
+app.include_router(router)
+
+@app.get("/",summary="health check endpoint")
+def health_check():
+  return {"status":"ok","message":"Faculty Finder API is running"}
